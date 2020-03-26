b04942986a96c031aea0f835fbd10b4e61674445
Christopher Reinken <christopherreinken@gmail.com>
2020-03-26 00:16:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  flipped_words = sentence_array.each {|word| word.reverse})
+  flipped_words = sentence_array.each {|word| word.reverse}
   new_sentence_string = flipped_words.join( " " )
   return new_sentence_string
 end
