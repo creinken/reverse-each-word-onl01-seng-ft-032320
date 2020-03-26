7168309b6c7b5651587d336b79dba0f114a3ec75
Christopher Reinken <christopherreinken@gmail.com>
2020-03-26 00:13:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   new_sentence_string = ""
-  new_sentence_string sentence_array.each do |word|
-    word.reverse + " "
-  end
+  new_sentence_string.join( " " sentence_array.each {|word| word.reverse})
   return new_sentence_string
 end
